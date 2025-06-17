532247f31b49a2825e9418b4f8f6e52c6b2a94f1
phanirithvij <phanirithvij2000@gmail.com>
2025-06-17 09:45:57 +0530

    fixup! nix flake init

--- nix/go-maven-resolver.nix
+++ nix/go-maven-resolver.nix
@@ -1,21 +1,26 @@
-{ buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
 
 buildGoModule (finalAttrs: {
   pname = "go-maven-resolver";
-  version = "1.1.2";
+  version = "1.1.2-unstable-2025-06-16";
 
   vendorHash = "sha256-dlqI+onfeo4tTwmHeq8heVKRzLU1gFEQ+4iv+8egN90=";
 
   src = fetchFromGitHub {
     owner = "status-im";
     repo = "go-maven-resolver";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-S7VyuRNyF+JepN0dN3hkZEsFIndNhwqO7u1fjXj5eFw=";
+    rev = "473b36df1d12996fc5fbcb8b7cc4f60c9aa4f8e0";
+    hash = "sha256-wYGjOcNnhMU2hwKGNLEAT4CcienKw5CvWieH1wV7bA8=";
   };
 
   meta = {
     description = "go maven resolver";
     homepage = "https://github.com/status-im/go-maven-resolver";
+    license = lib.licenses.mit;
     mainProgram = "go-maven-resolver";
   };
 })
